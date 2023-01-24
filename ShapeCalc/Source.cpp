2357d148accdbea1c29a4b4f7d147c347c72adde
Sabriel <systeminabox28@gmail.com>
2023-01-24 15:24:38 -0800

    Init

---
+++ ShapeCalc/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void main() {
+	/*TODO
+	* create menu of 5 shapes
+	* have user input needed dimensions for area
+	* display shape using console ASCII text art? (look into generating this for myself)
+	* calculate area and display
+	*/
+}
